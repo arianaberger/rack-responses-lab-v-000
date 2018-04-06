9bfa6dbaeef07d6c7d71fd4b04eaa5f31f675513
Ariana Berger <ariana.berger@gmail.com>
2018-04-06 01:01:52 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     current_time = Time.now.to_s
     time = current_time[11,3].to_i
 
-    if time > 12
+    if time >= 12
       resp.write "Good Afternoon!"
     else
       resp.write "Good Morning!"
