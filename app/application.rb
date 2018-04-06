70237b4e33e59c793ba361f58e9bf9715c0bb8f5
Ariana Berger <ariana.berger@gmail.com>
2018-04-06 01:06:40 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,6 +4,9 @@ class Application
     resp = Rack::Response.new
     current_time = Time.now.to_s
     time = current_time[11,3].to_i
+    #you can also just call .hour on the current time to get the hour!
+    #current_time = Time.now
+    #current_time.hour >> 12 or 6, for example
 
     if time > 12
       resp.write "Good Afternoon!"
